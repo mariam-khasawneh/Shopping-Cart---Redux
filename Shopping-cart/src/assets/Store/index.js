517e1c91800590a8bce5c7f266d664aa0e3b2273
mariam-khasawneh <khasawnehmariam@gmail.com>
2024-09-11 20:54:24 +0300

    done

--- Shopping-cart/src/assets/Store/index.js
+++ Shopping-cart/src/assets/Store/index.js
@@ -1,4 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+// configureStore: هذه الدالة تأتي من Redux Toolkit
+// تستخدم لإنشاء المتجر الذي يجمع جميع الـ reducers الخاصة بالتطبيق.
+
+//================================================
 import cartReducer from "./cartSlice";
 import productsReducer from "./productsSlice";
 
